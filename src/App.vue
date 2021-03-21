9b5a4c760467e07bdb227849c44302c95d0cc4aa
88Hannah <hannahsangar@gmail.com>
2021-03-21 14:55:24 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Initial set up
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
